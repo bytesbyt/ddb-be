bd5be949ca211c9bc724a33fdd107117f0e62d15
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-02 19:55:37 +0100

    update user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,29 +1,34 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
-const userController = {}
+const userController = {};
 
 userController.createUser = async (req, res) => {
-    try {
-        let { email, password, name, level } = req.body;
-        const user = await User.findOne({email})
-        if(user) {
-            throw new Error({message: "User already exists"});
-        }
-        const salt = await bcrypt.genSaltSync(10);
-        password = await bcrypt.hash(password, salt);
-        const newUser = new User({email, password, name, level: level?level:"customer"})
-        await newUser.save();
-        return res.status(201).json({
-            status: "success",
-            message: "User created successfully",
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: "fail",
-            error: error.message,
-        })
+  try {
+    let { email, password, name, level } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw new Error({ status: "fail", message: "User already exists" });
     }
-}
+    const salt = await bcrypt.genSaltSync(10);
+    password = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      email,
+      password,
+      name,
+      level: level ? level : "customer",
+    });
+    await newUser.save();
+    return res.status(201).json({
+      status: "success",
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: error.message,
+    });
+  }
+};
 
 module.exports = userController;

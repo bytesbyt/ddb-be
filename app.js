72c9ab37a0671854529a3dfbe2cb758dc6da36e4
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-02 16:54:39 +0100

    connect to mongodb

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("DB connection fail", err));
+
+app.listen(process.env.PORT || 5002, () => {
+  console.log("Server is running on port 5002");
+});
+

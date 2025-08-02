5dd433450075ff06e6e0183356c6ab5beddeba73
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-02 17:13:38 +0100

    add schema

---
+++ model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    default: "customer",
+  },
+}, { timestamps: true });
+userSchema.methods.toJSON = function() {
+    const obj = this._doc
+    delete obj.password
+    delete obj.__v
+    delete obj.createdAt
+    delete obj.updatedAt
+    return obj
+}
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

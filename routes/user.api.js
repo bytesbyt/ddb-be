483d109e388c241b06895707673ec5717f869de9
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-03 19:08:45 +0100

    add token login

--- routes/user.api.js
+++ routes/user.api.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.post("/", userController.createUser);
+router.get("/me", authController.authenticate, userController.getUser); //checking if the token is valid first (middleware)
+
 
 module.exports = router;

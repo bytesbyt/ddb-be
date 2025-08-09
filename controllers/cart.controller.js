96b8569aeeeb560815046bb0267d8b18b34ae30e
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-09 06:53:52 +0100

    add edit and delete feature - cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -55,4 +55,47 @@ cartController.getCartQty = async (req, res) => {
   }
 };
 
+cartController.deleteCartItem = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      throw new Error("Cart not found");
+    }
+    cart.items = cart.items.filter((item) => !item._id.equals(id));
+    await cart.save();
+    res.status(200).json({ status: "success", cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.updateCartItemQty = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+    const { qty } = req.body;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    if (!cart) {
+      throw new Error("Cart not found");
+    }
+    const item = cart.items.find((item) => item._id.equals(id));
+    if (!item) {
+      throw new Error("Item not found in cart");
+    }
+    item.qty = qty;
+    await cart.save();
+    res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = cartController;
--- routes/cart.api.js
+++ routes/cart.api.js
@@ -6,5 +6,7 @@ const authController = require("../controllers/auth.controller");
 router.post("/", authController.authenticate, cartController.addItemToCart);
 router.get("/", authController.authenticate, cartController.getCart);
 router.get("/qty", authController.authenticate, cartController.getCartQty);
+router.put("/:id", authController.authenticate, cartController.updateCartItemQty);
+router.delete("/:id", authController.authenticate, cartController.deleteCartItem);
 
 module.exports = router;
